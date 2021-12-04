cb0bf52a7eb816049fcee670053618a79321aa8d
hideto tsuzuki <ma_kux@icloud.com>
2021-12-05 01:45:58 +0900

    new commit

---
+++ rest/db_mysql/mysql_connection.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+mysql = mysql.connector.connect(
+    host='localhost',
+    user='hideto tsuzuki',
+    password='Xmaku2500',
+    password='Xmaku2500',
+    post='3306',
+    database='python_mysql'
+)
