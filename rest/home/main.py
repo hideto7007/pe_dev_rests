de54d40fcfa6dd91c256077d74473d4ea64dfe87
hideto tsuzuki <ma_kux@icloud.com>
2021-12-05 00:32:34 +0900

    Initial commit

---
+++ rest/home/main.py
@@ -0,0 +1,15 @@
+# ホーム画面
+ids = 1000
+pas = 2000
+
+
+class Main_Home:
+    def __prepare__(ids, pas):
+        id = 1000
+        pa = 2000
+        if ids == id and pas == password:
+            print("ログインできます")
+        else:
+            print("ログインできません")
+
+
